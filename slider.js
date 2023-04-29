592a2c7e5537a97f0172767203c44d47e78d31bb
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-04-29 22:43:45 +0300

    Added transition

--- slider.js
+++ slider.js
@@ -3,12 +3,22 @@ const slider = {
 
   currentIndex: 0,
 
-  clickByItem(index) {},
+  clickByItem(index) {
+    this.currentIndex = index;
+    this.render();
+  },
 
   render() {
     this.items.forEach(function (item, index) {
       item.style.setProperty("--progress", index - this.currentIndex);
       item.style.setProperty("--active", index === this.currentIndex ? 1 : 0);
+
+      const opacity =
+        (3 * Math.abs(index - this.currentIndex)) / (this.items.length - 1);
+      item.style.setProperty(
+        "--opacity",
+        index === this.currentIndex ? 1 : opacity
+      );
     }, this);
   },
 
